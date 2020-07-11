cd230949eade4a353a7485caeeb1f8183814bbf7
Monica Nadal <missmonicanadal@gmail.com>
2020-07-10 18:27:34 -0700

    20200710 FE BE add item done

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,9 @@ export default {
         console.log(item)
         return fetch('/api/item', {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify(item)
         })
     }
