7de2ccd939e2bead939d34e4865628d1e39eb68c
Monica Nadal <missmonicanadal@gmail.com>
2020-07-18 14:08:40 -0700

    20200718 required path in server js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv');
 const express = require('express');
 // const mongoose = require('mongoose');
 const apiRoutes = require('./routes/API-routes');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
