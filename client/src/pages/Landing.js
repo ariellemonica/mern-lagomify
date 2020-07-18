fcc1bce32dd6367c6ab5177a38a9e06d144e5723
fmflores <frankthetank67@gmail.com>
2020-07-18 11:53:02 -0700

    auth-page commit 07182020

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { GoogleLogin } from 'react-google-login';
 
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
