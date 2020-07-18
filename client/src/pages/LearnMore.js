b48a274d8eb119d8e05796e57da558c7591a327c
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-17 18:50:28 -0700

    feat: add full implementation of learn more page
    - Page now reads from database via the API, rather than a simple JSON file.
    - Page is now implemented with React Hooks to accommodate Material UI.
    
    - [ ] Resolve network race condition.
    - [ ] Resolve warning re unique `key` property on child list items.

--- client/src/pages/LearnMore.js
+++ client/src/pages/LearnMore.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Typography } from '@material-ui/core';
 import { Main, ResourceCard } from '../components';
-import resources from '../data/resources.json';
+import API from '../utils/API';
 
 const useStyles = makeStyles((theme) => ({
   stretch: {
@@ -11,20 +11,14 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function LearnMore () {
+const LearnMore = () => {
+  const [resources, setResources] = useState([]);
   const classes = useStyles();
 
-  const extResources = resources.map(resource =>
-    <Grid item key={resource.id} xs={6} className={classes.stretch}>
-      <ResourceCard
-        id={resource.id}
-        title={resource.title}
-        link={resource.link}
-        img={resource.image}
-        alt={resource.alt}
-      />
-    </Grid>
-  );
+  API.getResources()
+    .then((resp) => resp.json())
+    .then((data) => setResources(data))
+    .catch(err => console.error(err.stack));
 
   return (
     <Main>
@@ -33,10 +27,32 @@ export default function LearnMore () {
           <Typography variant="h2">Learn More</Typography>
         </Grid>
         <Grid item xs={12}>
-          <Typography variant="body1">The following resources offer more information regarding minimalism and simplifying your life. Peruse them to find out how to make room in your life for what really matters.</Typography>
+          <Typography variant="body1">
+            The following resources offer more information regarding minimalism and simplifying your life. Peruse them to find out how to make room in your life for what really matters.
+          </Typography>
         </Grid>
-        {extResources}
+        { resources.length ? (
+          resources.map(resource => (
+            <Grid item key={resource.id} xs={6} className={classes.stretch}>
+              <ResourceCard
+                key={resource.id}
+                title={resource.title}
+                link={resource.link}
+                img={resource.image}
+                alt={resource.alt}
+              />
+            </Grid>
+          ))
+        ) : (
+          <Grid item xs={12}>
+            <Typography variant="body1">
+              There are no external resources available at this time. Please check back later.
+            </Typography>
+          </Grid>
+        )}
       </Grid>
     </Main>
   );
-}
+};
+
+export default LearnMore;
