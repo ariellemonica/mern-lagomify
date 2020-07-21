97ff04efdd031c1575bc65a328ca85986eb0627e
Monica Nadal <missmonicanadal@gmail.com>
2020-07-20 18:39:57 -0700

    20200720 tied item added to logged in user

--- client/src/pages/ItemAdd.js
+++ client/src/pages/ItemAdd.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { authContext } from '../utils/appContext';
+// import { authContext } from '../utils/appContext';
 // import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button, Typography } from '@material-ui/core';
 import { Main } from '../components';
