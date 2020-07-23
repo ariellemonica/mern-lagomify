f69393127ca80830e82423fea5f02c8cb86fd15a
Christopher Zenner <christopher.zenner@gmail.com>
2020-07-23 16:34:44 -0700

    style: add styling to site greeting

--- client/src/pages/Member.js
+++ client/src/pages/Member.js
@@ -1,16 +1,44 @@
 import React, { useContext } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 import { authContext } from '../utils/appContext';
 import ItemAdd from '../pages/ItemAdd';
 
-// the user? div around the imported pages/components is necessary because we don't want them to load before authentication is done
+// NOTE: the user? div around the imported pages/components is necessary because we don't want them to load before authentication is done
+
+const useStyles = makeStyles((theme) => ({
+  greeting: {
+    fontWeight: 'bold',
+    marginRight: theme.spacing(4)
+  },
+  username: {
+    // ...theme.typography.button,
+    // color: '#d29291'
+    color: '#115293'
+  }
+}));
 
 export default () => {
   const { user, loading } = useContext(authContext);
+  const classes = useStyles();
+
+  // DEBUG:
   console.log(user, loading);
-  return (<>
-    <h1>HELLO {user?.name}</h1>
-    {user ? <div>
-      <ItemAdd user = {user}/>
-    </div> : <div>Loading user information ... </div>}
-  </>);
+
+  return (
+    <>
+      <Typography variant="body1"
+        align="right"
+        className={classes.greeting}>
+        Hi, <span className={classes.username}>{user?.name}</span>!
+      </Typography>
+      { user
+        ? <Typography variant="body1">
+          <ItemAdd user={user || 'Guest'}/>
+        </Typography>
+        : <Typography variant="body1">
+          Loading user information ...
+        </Typography>}
+    </>
+  );
 };
